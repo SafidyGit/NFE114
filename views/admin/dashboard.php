53ff1bbbb0dcf4cf722813648b2dd243454d9b59
Henintsoa RAZAFINIME <henintsoarzfm@gmail.com>
2025-06-12 18:46:47 +0300

    totalProductsByCategory

--- views/admin/dashboard.php
+++ views/admin/dashboard.php
@@ -21,58 +21,57 @@ include 'layout/header.php';
     </button>
 
     <h3>Bienvenue <?php echo $_SESSION['username'];?></h3>
-    <div class="row">
-      <div class="bg-secondary col col-md-3 text-white p-4 rounded shadow d-flex justify-content-between align-items-center mb-4 m-3" style="min-width: 220px;">
-        <div>
-          <h6 class="text-uppercase fw-bold mb-1">Nombre de produits</h6>
-          <h3 class="m-0"><?= $count_product_in_stock ?? '';?></h3>
+      <div class="table-responsive bg-dark p-3 rounded shadow">
+          <div class="row">
+        <div class="bg-secondary col col-md-3 text-white p-4 rounded shadow d-flex justify-content-between align-items-center mb-4 m-3" style="min-width: 220px;">
+          <div>
+            <h6 class="text-uppercase fw-bold mb-1">Nombre de produits</h6>
+            <h3 class="m-0"><?= $count_product_in_stock ?? '';?></h3>
+          </div>
+          <div>
+            <i class="bi bi-pc-display-horizontal fs-1 text-light"></i>
+          </div>
         </div>
-        <div>
-          <i class="bi bi-pc-display-horizontal fs-1 text-light"></i>
+        <div class="bg-secondary col col-md-3 text-white p-4 rounded shadow d-flex justify-content-between align-items-center mb-4 m-3" style="min-width: 220px;">
+          <div>
+            <h6 class="text-uppercase fw-bold mb-1">Nombre de produits epuisés</h6>
+            <h3 class="m-0"><?= $out_of_stock ?? '';?></h3>
+          </div>
+          <div>
+            <i class="bi bi-dash-circle fs-1 text-danger"></i>
+          </div>
+        </div>
+        <div class="bg-secondary col col-md-3 text-white p-4 rounded shadow d-flex justify-content-between align-items-center mb-4 m-3" style="min-width: 220px;">
+          <div>
+            <h6 class="text-uppercase fw-bold mb-1">Produits sous le seuil</h6>
+            <h3 class="m-0"><?= $alert_stock ?? '';?></h3>
+          </div>
+          <div>
+            <i class="bi bi-exclamation-triangle-fill fs-1 text-warning"></i>
+          </div>
         </div>
       </div>
-      <div class="bg-secondary col col-md-3 text-white p-4 rounded shadow d-flex justify-content-between align-items-center mb-4 m-3" style="min-width: 220px;">
-        <div>
-          <h6 class="text-uppercase fw-bold mb-1">Nombre de produits epuisés</h6>
-          <h3 class="m-0"><?= $out_of_stock ?? '';?></h3>
-        </div>
-        <div>
-          <i class="bi bi-dash-circle fs-1 text-danger"></i>
-        </div>
-      </div>
-      <div class="bg-secondary col col-md-3 text-white p-4 rounded shadow d-flex justify-content-between align-items-center mb-4 m-3" style="min-width: 220px;">
-        <div>
-          <h6 class="text-uppercase fw-bold mb-1">Produits sous le seuil</h6>
-          <h3 class="m-0"><?= $alert_stock ?? '';?></h3>
-        </div>
-        <div>
-          <i class="bi bi-exclamation-triangle-fill fs-1 text-warning"></i>
-        </div>
-      </div>
-    </div>
 
-    
-  <div class="table-responsive bg-dark p-3 rounded shadow">
-      <table class="table table-dark table-striped table-hover table-bordered mb-0">
-      <thead>
-      <tr>
-          <th>Catégorie</th>
-          <th>Total</th>
-      </tr>
-      </thead>
-      <?php if(!empty($totalProductsByCategory)):?>
-      <tbody>    
-      <?php foreach($totalProductsByCategory as $product) : ?>
-          <tr>
-              <td><?= $product['category']; ?></td>
-              <td><?= $product['Total']; ?></td>
-          </tr>
-          
-      <?php endforeach; ?>
-      </tbody>
-      <?php endif;?>
-      </table>
-  </div>
+        <table class="table table-dark table-striped table-hover table-bordered mb-0">
+        <thead>
+        <tr>
+            <th>Catégorie</th>
+            <th>Total</th>
+        </tr>
+        </thead>
+        <?php if(!empty($totalProductsByCategory)):?>
+        <tbody>    
+        <?php foreach($totalProductsByCategory as $product) : ?>
+            <tr>
+                <td><?= $product['category']; ?></td>
+                <td><?= $product['Total']; ?></td>
+            </tr>
+            
+        <?php endforeach; ?>
+        </tbody>
+        <?php endif;?>
+        </table>
+    </div>
 
     
 </div>
